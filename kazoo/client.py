62d60562a3556efb6a8195f6e92e236bc5202dbf
Mike Lundy <mike@fluffypenguin.org>
2013-06-05 13:43:05 -0700

    kill off retry sleeper

--- kazoo/client.py
+++ kazoo/client.py
@@ -204,10 +204,8 @@ class KazooClient(object):
                 sleep_func=self.handler.sleep_func,
                 **retry_keys)
 
-        self.retry_sleeper = self.retry.retry_sleeper.copy()
-
         self._connection = ConnectionHandler(
-            self, self.retry.retry_sleeper.copy(),
+            self, self.retry.copy(),
             logger=self.logger)
 
         # convenience API
